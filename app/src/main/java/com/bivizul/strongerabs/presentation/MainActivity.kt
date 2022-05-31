df7ea42578a7e25e0ddeaac72cafc7aec4798a73
Bivizul <mamochapa@gmail.com>
2022-05-31 14:10:54 +0200

    Init commit

---
+++ app/src/main/java/com/bivizul/strongerabs/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bivizul.strongerabs.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bivizul.strongerabs.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
